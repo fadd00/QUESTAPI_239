65abfdd22a27e5f3d12a4bd95e331fb32ee819d1
fadd00 <andhikahutama9@gmail.com>
2025-12-18 09:17:38 +0700

    amen

--- app/src/main/java/com/sample/prak12/modeldata/DataSiswa.kt
+++ app/src/main/java/com/sample/prak12/modeldata/DataSiswa.kt
@@ -25,3 +25,7 @@ fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
     alamat = alamat,
     telpon = telpon,
 )
+fun DataSiswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
