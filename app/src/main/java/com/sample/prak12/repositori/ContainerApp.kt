3dee9710831c68c340f34160b3dd1ec2a8ed69a7
fadd00 <andhikahutama9@gmail.com>
2025-12-18 17:43:29 +0700

    amen

--- app/src/main/java/com/sample/prak12/repositori/ContainerApp.kt
+++ app/src/main/java/com/sample/prak12/repositori/ContainerApp.kt
@@ -1,17 +1,20 @@
 package com.sample.prak12.repositori
 
-interface ContainerApi{
-    val repositoriDataSiswa : repositoriDataSiswa
+interface ContainerApp {
+    val repositoryDataSiswa: RepositoryDataSiswa
 }
-class DefaultAppContainer : ContainerApi {
-    private val baseUrl = ""
+
+class DefaultContainerApp : ContainerApp {
+    private val baseurl = "http://10.0.2.2/PAM_act8_backendAPI/"
 
     val logging = HttpLoggingInterceptor().apply{
         level = HttpLoggingInterceptor.Level.BODY
-}
+    }
+
     val klien = OkHttpClient.Builder()
         .addInterceptor(logging)
         .build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(baseurl)
         .addConverterFactory(
@@ -21,13 +24,16 @@ class DefaultAppContainer : ContainerApi {
                 isLenient = true
             }.asConverterFactory("application/json".toMediaType())
         ).build()
+
     private val retrofitService: ServiceApiSiswa by lazy {
         retrofit.create(ServiceApiSiswa::class.java)
     }
+
     override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
         JaringanRepositoryDataSiswa(retrofitService)
     }
 }
+
 class AplikasiDataSiswa : Application() {
     lateinit var container : ContainerApp
     override fun onCreate() {
