005bde6dd5e0b2ebab49afffc1ac6a83d7219c00
fadd00 <andhikahutama9@gmail.com>
2025-12-18 17:40:09 +0700

    amen

--- app/src/main/java/com/sample/prak12/apiservice/serviceapisiswa.kt
+++ app/src/main/java/com/sample/prak12/apiservice/serviceapisiswa.kt
@@ -1,2 +1,9 @@
 package com.sample.prak12.apiservice
 
+interface ServiceApiSiswa {
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+}
