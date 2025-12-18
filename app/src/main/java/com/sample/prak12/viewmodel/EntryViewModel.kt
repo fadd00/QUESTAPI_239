4704a5d610203df2a509c6df207d91e7b4ae7ad8
fadd00 <andhikahutama9@gmail.com>
2025-12-18 18:42:23 +0700

    amen

--- app/src/main/java/com/sample/prak12/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/sample/prak12/viewmodel/EntryViewModel.kt
@@ -1,2 +1,36 @@
 package com.sample.prak12.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.sample.prak12.modeldata.DetailSiswa
+import com.sample.prak12.modeldata.UIStateSiswa
+import com.sample.prak12.repositori.RepositoryDataSiswa
+import okhttp3.Response
+
+class EntryViewModel (private val repositoryDataSiswa: RepositoryDataSiswa):
+    ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa):
+            Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun  updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            val sip: Response<Void> = repositoryDataSiswa.postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+            if (sip.isSuccessful) {
+                println("Sukses Tambah Data: ${sip.message()}")
+            }else{
+                println("Gagal Tambah Data: ${sip.errorBody()}")
+            }
+        }
+    }
+}
--- app/src/main/java/com/sample/prak12/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/sample/prak12/viewmodel/HomeViewModel.kt
@@ -1,2 +1,31 @@
 package com.sample.prak12.viewmodel
 
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
+    object Error: StatusUiSiswa
+    object Loading: StatusUiSiswa
+}
+class HomeViewModel (private val repositoryDataSiswa: RepositoryDataSiswa):
+    ViewModel() {
+    var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        loadSiswa()
+    }
+
+    fun loadSiswa() {
+        viewModelScope.launch {
+            listSiswa = StatusUiSiswa.Loading
+            listSiswa = try {
+                StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
+            }
+            catch (e: IOException) {
+                StatusUiSiswa.Error
+            }
+            catch (e: HttpException) {
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
