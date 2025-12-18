67d6a89127c423e08dad468365a55c91a0c3ad8a
fadd00 <andhikahutama9@gmail.com>
2025-12-18 09:30:51 +0700

    amen

--- app/src/main/java/com/sample/prak12/repositori/kontainerapi.kt
+++ app/src/main/java/com/sample/prak12/repositori/kontainerapi.kt
@@ -3,3 +3,5 @@ package com.sample.prak12.repositori
 interface ContainerApi{
     val repositoriDataSiswa : repositoriDataSiswa
 }
+class DefaultAppContainer : ContainerApi{
+    private val baseUrl =
