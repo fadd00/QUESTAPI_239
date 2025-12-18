952097ea73d2885e3357c43304e2584a048686e3
fadd00 <andhikahutama9@gmail.com>
2025-12-18 18:10:30 +0700

    amen

--- app/src/main/java/com/sample/prak12/repositori/ContainerApp.kt
+++ app/src/main/java/com/sample/prak12/repositori/ContainerApp.kt
@@ -1,5 +1,14 @@
 package com.sample.prak12.repositori
 
+import android.app.Application
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.sample.prak12.apiservice.ServiceApiSiswa
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
 interface ContainerApp {
     val repositoryDataSiswa: RepositoryDataSiswa
 }
